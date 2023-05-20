a6e92b6576c61cc47b82d21a8b2b67d02056dc48
Pavel <lmr_16@mail.ru>
2023-05-20 20:44:08 +0300

    added custom NavBar

--- WorkoutApp/Controllers/Overview/Views/OverviewNavBar.swift
+++ WorkoutApp/Controllers/Overview/Views/OverviewNavBar.swift
@@ -8,6 +8,29 @@
 import UIKit
 
 final class OverviewNavBar: BaseView {
+    
+    private let titleLabel = UILabel()
+    private let allWorkoutButton = SecondaryButton()
+    private let addButton = UIButton()
+    
+    private let weekView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue.withAlphaComponent(0.2)
+        return view
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        addBottomBorder(with: Resources.Colors.separator, height: 1)
+    }
+    
+    func addAllWorkoutsAction(_ action: Selector, with target: Any?) {
+        allWorkoutButton.addTarget(target, action: action, for: .touchUpInside)
+    }
+    
+    func additingAction(_ action: Selector, with target: Any?) {
+        addButton.addTarget(target, action: action, for: .touchUpInside)
+    }
 
 }
 
@@ -15,16 +38,53 @@ extension OverviewNavBar {
     override func addViews() {
         super.addViews()
         
+        addView(titleLabel)
+        addView(allWorkoutButton)
+        addView(addButton)
+        addView(weekView)
+        
     }
     
     override func layoutViews() {
         super.layoutViews()
         
+        NSLayoutConstraint.activate([
+            addButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8),
+            addButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
+            addButton.heightAnchor.constraint(equalToConstant: 28),
+            addButton.widthAnchor.constraint(equalToConstant: 28),
+            
+            allWorkoutButton.topAnchor.constraint(equalTo: addButton.topAnchor),
+            allWorkoutButton.trailingAnchor.constraint(equalTo: addButton.leadingAnchor, constant: -15),
+            allWorkoutButton.heightAnchor.constraint(equalToConstant: 28),
+            allWorkoutButton.widthAnchor.constraint(equalToConstant: 130),
+            
+            titleLabel.centerYAnchor.constraint(equalTo: allWorkoutButton.centerYAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: allWorkoutButton.leadingAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            
+            weekView.topAnchor.constraint(equalTo: addButton.bottomAnchor, constant: 15),
+            weekView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            weekView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
+            weekView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -15),
+            weekView.heightAnchor.constraint(equalToConstant: 47)
+        ])
+  
     }
     
     override func configure() {
         super.configure()
         
+        backgroundColor = .white
+
+        titleLabel.text = Resources.Strings.NavBar.overview
+        titleLabel.textColor = Resources.Colors.titleGray
+        titleLabel.font = Resources.Fonts.helvelticaRegular(with: 22)
+
+        allWorkoutButton.setTitle(Resources.Strings.Overview.allWorkoutsButton)
+
+        addButton.setImage(Resources.Images.Common.add, for: .normal)
+
     }
 }
 
---
+++ WorkoutApp/Controllers/Progress/ProgressController.swift
@@ -0,0 +1,22 @@
+//
+//  ProgressController.swift
+//  WorkoutApp
+//
+//  Created by user on 19.05.2023.
+//
+
+import UIKit
+
+class ProgressController: BaseController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = Resources.Strings.NavBar.progress
+        navigationController?.tabBarItem.title = Resources.Strings.TabBar.progress
+        
+        addNavBarButton(at: NavBarPosition.left, with: Resources.Strings.Progress.navBarLeft)
+        addNavBarButton(at: NavBarPosition.right, with: Resources.Strings.Progress.navBarRight)
+    }
+
+}
