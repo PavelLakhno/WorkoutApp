1dcb4e3b4591f132af81a0fc5553a42e6fe6abbe
Pavel <lmr_16@mail.ru>
2023-05-25 17:09:34 +0300

    added BezierPath for circle ProgressView

--- WorkoutApp/Controllers/Session/Views/Timer/ProgressView.swift
+++ WorkoutApp/Controllers/Session/Views/Timer/ProgressView.swift
@@ -10,7 +10,27 @@ import UIKit
 extension TimerView {
     final class ProgressView: UIView {
         func drawProgress(with precent: CGFloat) {
+            let circleFrame = UIScreen.main.bounds.width - (15 + 40) * 2
+            let radius = circleFrame / 2
+            let center = CGPoint(x: radius, y: radius)
+            let startAngle = -CGFloat.pi * 7 / 6
+            let endAngle = CGFloat.pi * 1 / 6
+
+            let circlePath = UIBezierPath(arcCenter: center,
+                                          radius: radius,
+                                          startAngle: startAngle,
+                                          endAngle: endAngle,
+                                          clockwise: true)
+
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.path = circlePath.cgPath
+            shapeLayer.strokeColor = Resources.Colors.active.cgColor
+            shapeLayer.lineWidth = 20
+            shapeLayer.strokeEnd = precent
+            shapeLayer.fillColor = UIColor.clear.cgColor
+            shapeLayer.lineCap = .round
             
+            layer.addSublayer(shapeLayer)
         }
     }
 }
--- WorkoutApp/Controllers/Session/Views/Timer/TimerView.swift
+++ WorkoutApp/Controllers/Session/Views/Timer/TimerView.swift
@@ -10,7 +10,7 @@ import UIKit
 final class TimerView: WABaseInfoView {
     private let progressView: ProgressView = {
         let view = ProgressView()
-        view.drawProgress(with: 0)
+        view.drawProgress(with: 0.7)
         return view
     }()
     
@@ -40,6 +40,6 @@ extension TimerView {
     override func configureAppearance() {
         super.configureAppearance()
 
-        progressView.backgroundColor = .red
+        //progressView.backgroundColor = .red
     }
 }
