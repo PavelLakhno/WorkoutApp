909f09dfaf137030a9570cca221321fdda12efe2
Pavel <lmr_16@mail.ru>
2023-05-25 15:41:21 +0300

    start to create animation of ProgressVIew circle

---
+++ WorkoutApp/Controllers/Session/Views/Timer/ProgressView.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressView.swift
+//  WorkoutApp
+//
+//  Created by user on 25.05.2023.
+//
+
+import UIKit
+
+extension TimerView {
+    final class ProgressView: UIView {
+        func drawProgress(with precent: CGFloat) {
+            
+        }
+    }
+}
+
