01bd51300fd90985b7b1605e9c96bd0ae08251ea
Pavel <lmr_16@mail.ru>
2023-05-31 15:26:37 +0300

    added animation of labels at timer

--- WorkoutApp/Controllers/Session/SessionController.swift
+++ WorkoutApp/Controllers/Session/SessionController.swift
@@ -11,7 +11,7 @@ class SessionController: BaseController {
 
     private let timerView = TimerView()
 
-    private let timerDuration = 3.0
+    private let timerDuration = 3614.0
     
     override func navBarLeftButtonHandler() {
         if timerView.state == .isStopped {
--- WorkoutApp/Controllers/Session/Views/Timer/TimerView.swift
+++ WorkoutApp/Controllers/Session/Views/Timer/TimerView.swift
@@ -74,6 +74,9 @@ final class TimerView: WABaseInfoView {
         let goalValueDevider = duration == 0 ? 1 : duration
         let percent = tempCurrentValue / goalValueDevider
         
+        elapsedTimeValueLabel.text = getDisplayedString(from: Int(tempCurrentValue))
+        remainingTimeValueLabel.text = getDisplayedString(from: Int(duration) - Int(tempCurrentValue))
+        
         progressView.drawProgress(with: CGFloat(percent))
     }
     
@@ -157,5 +160,21 @@ extension TimerView {
         super.configureAppearance()
 
         //progressView.backgroundColor = .red
+    }
+}
+
+private extension TimerView {
+    func getDisplayedString(from value: Int) -> String {
+        let seconds = value % 60
+        let minutes = (value / 60) % 60
+        let hours = value / 3600
+
+        let secondsStr = seconds < 10 ? "0\(seconds)" : "\(seconds)"
+        let minutesStr = minutes < 10 ? "0\(minutes)" : "\(minutes)"
+        let hoursStr = hours < 10 ? "0\(hours)" : "\(hours)"
+
+        return hours == 0
+            ? [minutesStr, secondsStr].joined(separator: ":")
+            : [hoursStr, minutesStr, secondsStr].joined(separator: ":")
     }
 }
