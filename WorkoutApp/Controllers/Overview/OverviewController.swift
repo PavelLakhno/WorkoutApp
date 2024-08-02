0b073a7575b7a6b07fa764c1049c572b8c04a255
Pavel <lmr_16@mail.ru>
2024-08-02 17:20:24 +0300

    create title for tableView in Overview

--- WorkoutApp/Controllers/Overview/OverviewController.swift
+++ WorkoutApp/Controllers/Overview/OverviewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class OverviewController: BaseController {
     
     private let navBar = OverviewNavBar()
+    private let header = SectionHeaderView()
 }
 
 extension OverviewController {
@@ -17,6 +18,7 @@ extension OverviewController {
         super.setupViews()
         
         view.setupView(navBar)
+        view.setupView(header)
     }
     
     override func setupConstraintViews() {
@@ -26,11 +28,18 @@ extension OverviewController {
             navBar.topAnchor.constraint(equalTo: view.topAnchor),
             navBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             navBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            header.topAnchor.constraint(equalTo: navBar.bottomAnchor),
+            header.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -16),
+            header.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 16),
+            header.heightAnchor.constraint(equalToConstant: 32)
         ])
     }
     
     override func configureAppearance() {
         super.configureAppearance()
+        
+        header.configure(with: "test string")
         
         navigationController?.navigationBar.isHidden = true
     }
---
+++ WorkoutApp/Controllers/Overview/Views/CollectionView/Views/SectionHeaderView.swift
@@ -0,0 +1,53 @@
+//
+//  SectionHeaderView.swift
+//  WorkoutApp
+//
+//  Created by Pavel Lakhno on 02.08.2024.
+//
+
+import UIKit
+
+final class SectionHeaderView: UICollectionReusableView {
+    static let id = "SectionHeaderView"
+    
+    private let title: UILabel = {
+        let label = UILabel()
+        label.font = Resources.Fonts.helvelticaRegular(with: 13)
+        label.textAlignment = .center
+        label.textColor = Resources.Colors.inactive
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setupConstraintViews()
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with title: String) {
+        self.title.text = title
+    }
+}
+
+
+extension SectionHeaderView {
+    
+    func setupViews() {
+        setupView(title)
+    }
+    
+    func setupConstraintViews() {
+        
+        NSLayoutConstraint.activate([
+            title.centerXAnchor.constraint(equalTo: centerXAnchor),
+            title.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+
+}
